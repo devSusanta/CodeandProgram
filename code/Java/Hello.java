76fd2731ebba4b683decddf3403f8ad153f43864
Susanta Mandi <130727297+devSusanta@users.noreply.github.com>
2023-04-15 19:36:55 +0530

    Create Hello.java

---
+++ code/Java/Hello.java
@@ -0,0 +1,5 @@
+public class Hello{
+	public static void(String args[]){
+		System.out.print("in this Java directory i write problem and solution of java code that are usualy pjrogrammer faces while coding");
+	}
+}
